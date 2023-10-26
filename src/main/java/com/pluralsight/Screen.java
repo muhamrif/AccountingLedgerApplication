955a7326961c89900eef4dd265ed4328749143cd
Muhammad Hamza <muhammad.hamza6415@gmail.com>
2023-10-26 12:43:45 -0400

    fixed imports

--- src/main/java/com/pluralsight/Screen.java
+++ src/main/java/com/pluralsight/Screen.java
@@ -1,17 +1,8 @@
 package com.pluralsight;
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
-import java.io.File;
-import java.io.FileReader;
-import java.io.FileWriter;
-import java.io.IOException;
-import java.time.LocalDate;
-import java.time.LocalDateTime;
-import java.time.LocalTime;
+
 import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
 import java.util.Scanner;
-import java.util.*;
 
 /**
  *
